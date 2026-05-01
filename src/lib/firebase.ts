54181c2c546214320bde84bd96d0a7a37602b01e
BANGBER <166294794+BANGBER@users.noreply.github.com>
2026-05-02 01:43:29 +0700

    Update Firebase configuration with correct environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,8 +5,8 @@ import { getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../../firebase-applet-config.json';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_API_KEY,
-  projectId: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
 };
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
