5abd4b4391c17f5cb2151874f6e6df2a4e9698b3
BANGBER <166294794+BANGBER@users.noreply.github.com>
2026-05-02 00:35:02 +0700

    Modify firebase configuration settings
    Updated firebase configuration with new API details.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,11 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 import firebaseConfig from '../../firebase-applet-config.json';
-
+const firebaseConfig = {
+  apiKey: "ISI_API_KEY",
+  authDomain: "ISI_DOMAIN",
+  projectId: "ISI_PROJECT_ID",
+};
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
